d761f07090f1810962e9e16171b2e36bc1ee346c
Sharath pavan Balla <suresh@yaksee.com>
2020-05-13 21:46:23 +0530

    commit - v0.0.1 dexter lib integrated for runtime permissions. and few changes to get the location based on the name.

--- app/src/main/java/com/sharath/location_map/MapsActivity.java
+++ app/src/main/java/com/sharath/location_map/MapsActivity.java
@@ -2,7 +2,11 @@ package com.sharath.location_map;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +14,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,6 +39,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        // enabling the current location button on map
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                })
+                .check();
     }
 
 
@@ -43,6 +80,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng hyderabad = new LatLng(17.3850, 78.4867);
         mMap.addMarker(new MarkerOptions().position(hyderabad).title("Marker in Hyderabad"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hyderabad, 10));
-        mMap.setMyLocationEnabled(true);
+        //mMap.setMyLocationEnabled(true);
+
+        geoLocate();
+    }
+
+    private void geoLocate() {
+        String search = "vijayawada";
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> addresses = new ArrayList<>();
+        try {
+            addresses = geocoder.getFromLocationName(search, 1);
+        } catch (Exception e) {}
+
+        if (addresses.size() > 0) {
+            Log.i("Map", addresses.toString());
+        }
+
     }
 }
